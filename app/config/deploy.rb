a159e72548934ee85aa077f2e9bf981f847300f5
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-17 14:52:49 +0000

    first commit

---
+++ app/config/deploy.rb
@@ -0,0 +1,70 @@
+# Deployment server info
+set :application, "SymfonyCapifony"
+set :domain,      "192.168.50.4"
+set :deploy_to,   "/home/vagrant/symfony"
+set :app_path,    "app"
+set :web_path, 	  "web"
+set :maintenance_basename, 	"maintenance"
+
+# User details for the production server
+default_run_options[:pty] = true
+set :user, "vagrant"
+set :use_sudo, false
+ssh_options[:forward_agent] = true
+set :ssh_options, {:forward_agent => true, keys: ['~/.vagrant.d/insecure_private_key']}
+ 
+# SCM info
+set :repository,  "file:///Users/ahdidou/Documents/ProjectsMobiblanc/ContinuousDeployement/SYMFONY/Symfony2/Symfony"
+set :deploy_via,  :copy
+set :scm,         :git
+ 
+set :model_manager, "doctrine"
+ 
+# Role info. I don't think this is particularly important for Capifony...
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                      # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+ 
+set :shared_files,      ["app/config/parameters.yml"] # This stops us from having to recreate the parameters file on every deploy.
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+
+set :use_composer, true
+set	:update_vendors, true
+
+# General config stuff
+set :keep_releases,  10
+set :permission_method, :acl
+
+task :upload_parameters do
+  origin_file = "app/config/parameters.yml"
+  destination_file = shared_path + "/app/config/parameters.yml" # Notice the
+  shared_path
+
+  try_sudo "mkdir -p #{File.dirname(destination_file)}"
+  top.upload(origin_file, destination_file)
+end
+
+after "deploy:setup", "upload_parameters"
+
+ 
+# The following line tells Capifony to deploy the last Git tag.
+# Since Jenkins creates and pushes a tag following a successful build this should always be the last tested version of the code.
+#set :branch, `git tag`.split("\n").last
+ 
+# Uncomment this if you need more verbose output from Capifony
+#logger.level = Logger::MAX_LEVEL
+ 
+# Run migrations before warming the cache
+#before "symfony:cache:warmup", "symfony:doctrine:migrations:migrate"
+ 
+# Custom(ised) tasks
+#namespace :deploy do
+#	# Apache needs to be restarted to make sure that the APC cache is cleared.
+#	# This overwrites the :restart task in the parent config which is empty.
+#	desc "Restart Apache"
+#	task :restart, :except => { :no_release => true }, :roles => :app do
+#		run "sudo service apache2 restart"
+#		puts "--> Apache successfully restarted".green
+#	end
+#end
