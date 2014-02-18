5d9de09d15e515fc33ba8201e24b75987b795519
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-18 09:17:23 +0000

    testing web hook

--- src/Blogger/BlogBundle/Controller/DefaultController.php
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name, $second)
+    public function indexAction($name, $second, $how)
     {
-        return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second));
+        return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second, 'how'=>$how));
     }
 }
