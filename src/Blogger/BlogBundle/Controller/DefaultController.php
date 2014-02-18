aff2fb8dc211c1690b4ec2631f180a62345b1561
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-18 11:13:49 +0000

    forcing duplicate code

--- src/Blogger/BlogBundle/Controller/DefaultController.php
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -13,4 +13,11 @@ class DefaultController extends Controller
         return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second, 'how'=>$how, 'day'=>$day));
 
     }
+
+    public function duplicate($name, $second, $how, $day)
+    {
+
+        return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second, 'how'=>$how, 'day'=>$day));
+
+    }
 }
