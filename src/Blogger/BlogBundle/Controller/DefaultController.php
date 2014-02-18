a061265fa256d6fbaddc43527bd7f40f4fdec9cd
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-18 09:20:12 +0000

    test

--- src/Blogger/BlogBundle/Controller/DefaultController.php
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction($name, $second, $how)
     {
+
         return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second, 'how'=>$how));
+        
     }
 }
