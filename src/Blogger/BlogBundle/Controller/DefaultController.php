e133d498afaa1e3cd1a6c893cda067fe2ceb3d54
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-18 09:36:50 +0000

    Comment added

--- src/Blogger/BlogBundle/Controller/DefaultController.php
+++ src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -4,12 +4,13 @@ namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+//Comment
 class DefaultController extends Controller
 {
     public function indexAction($name, $second, $how)
     {
 
         return $this->render('BloggerBlogBundle:Default:index.html.twig', array('name' => $name, 'second'=>$second, 'how'=>$how));
-        
+
     }
 }
